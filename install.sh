c7f205e5dcdd947032369999047b236e80aa819d
Austin Jackson <austinjackson892@gmail.com>
2017-09-13 15:04:59 +0000

    push before testing centos

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
+# ensure script runs as root
 if [[ $EUID -ne 0 ]]; then
    echo "Error: This script must be run as root." 
    exit 1
 fi
 
+# put jrsd in place
+mkdir /root/.bin
+cp ./jrsd.py /root/.bin
+chmod 755 /root/.bin/jrsd.py
+
+# put unit file in place
+cp ./jrsd.service /etc/systemd/system
+chmod 644 /etc/systemd/system/jrsd.service
+
+# put config file in place
+cp ./jrsd.conf /etc
+
+# create empty log file
+touch /var/log/jrsd.log
